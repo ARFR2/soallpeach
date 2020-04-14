b05353afece07efd651546d701d441f29a566818
A.R.F.R <arfr@a.r.f.r>
2020-04-14 16:04:17 +0430

    new code

--- prime/app.cpp
+++ prime/app.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 using namespace std;
 int main(int argc, char** argv)
 {
-
-    int num = 0;
-    ifstream infile(argv[1]);
-    while (infile >> num)
-    {
-
-        if ((num + 1) % 6 == 0 || num == 2 || num == 3 || (num - 1) % 6==0)
+	int num = 0;
+    bool prime=true;
+	ifstream infile(argv[1]);
+	while (infile >> num)
+	{
+        if(num == 2 || num == 3)
         {
-            cout << "1" <<endl;
+            cout << "1" << endl;
         }
-        else
-        {
-            cout << "0" << endl;
-        }
-    }
-    infile.close();
-    return 0;
+		else if ((num - 1) % 6 != 0 && (num + 1) % 6 != 0)
+		{
+			cout << "0" << endl;
+		}
+		else
+		{
+			for (int i = 2; i <= sqrt(num); i++)
+			{
+				if (num % i == 0)
+				{
+					cout << "0" << endl;
+                    prime=false;
+					break;
+				}
+                
+			}
+		}
+		if (prime=true)
+		{
+			cout << "1" << endl;
+		}
+	}
+	infile.close();
+	return 0;
 }
